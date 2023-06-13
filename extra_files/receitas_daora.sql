522e7b80531ef7cd0a56d010eae8de8c16205a7b
KauaDiniz6287 <125364596+KauaDiniz6287@users.noreply.github.com>
2023-06-12 22:12:26 -0300

    v0.1.2a

--- extra_files/receitas_daora.sql
+++ extra_files/receitas_daora.sql
@@ -1,6 +1,4 @@
 -- --------------------------------------------------------
--- Arquivo criado pela exportação do banco de dados pelo HeidiSQL
--- --------------------------------------------------------
 -- Servidor:                     127.0.0.1
 -- Versão do servidor:           10.4.28-MariaDB - mariadb.org binary distribution
 -- OS do Servidor:               Win64
@@ -16,11 +14,14 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
 -- Copiando estrutura do banco de dados para receitasdaora
+DROP DATABASE IF EXISTS `receitasdaora`;
 CREATE DATABASE IF NOT EXISTS `receitasdaora` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci */;
 USE `receitasdaora`;
 
 -- Copiando estrutura para tabela receitasdaora.bancoreceita
+DROP TABLE IF EXISTS `bancoreceita`;
 CREATE TABLE IF NOT EXISTS `bancoreceita` (
   `idReceita` int(11) NOT NULL AUTO_INCREMENT,
   `nome` text NOT NULL,
@@ -28,22 +29,31 @@ CREATE TABLE IF NOT EXISTS `bancoreceita` (
   `ingredientes` text NOT NULL,
   `modoPreparo` text NOT NULL,
   `tempoPreparo` int(11) NOT NULL,
+  `foto` varchar(255) DEFAULT 'nophoto.png',
   PRIMARY KEY (`idReceita`),
   KEY `fk_bancoReceita_bancoUsuario_idx` (`autor`),
   CONSTRAINT `fk_bancoReceita_bancoUsuario` FOREIGN KEY (`autor`) REFERENCES `bancousuario` (`idUsuario`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- Copiando dados para a tabela receitasdaora.bancoreceita: ~1 rows (aproximadamente)
+INSERT INTO `bancoreceita` (`idReceita`, `nome`, `autor`, `ingredientes`, `modoPreparo`, `tempoPreparo`, `foto`) VALUES
+	(1, 'Teste', 1, 'a; va; ca; da; fa; ga', 'wjfiejwfwifoejifjeiofjeiwofjeifjioewjfiodsifjojsioxjfkkmfenfejiofjeiofhjeofhbueihfu', 12, 'nophoto.png');
 
 -- Copiando estrutura para tabela receitasdaora.bancousuario
+DROP TABLE IF EXISTS `bancousuario`;
 CREATE TABLE IF NOT EXISTS `bancousuario` (
   `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(60) NOT NULL,
   `email` varchar(200) NOT NULL,
   `senha` varchar(200) NOT NULL,
-  `formacao` text DEFAULT '"N/A"',
+  `formacao` text DEFAULT NULL,
+  `foto` varchar(255) DEFAULT 'nophoto.png',
   PRIMARY KEY (`idUsuario`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
--- Exportação de dados foi desmarcado.
+-- Copiando dados para a tabela receitasdaora.bancousuario: ~1 rows (aproximadamente)
+INSERT INTO `bancousuario` (`idUsuario`, `nome`, `email`, `senha`, `formacao`, `foto`) VALUES
+	(1, 'admin', 'admin@mail.com', '123', 'Mestrado', 'nophoto.png');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
