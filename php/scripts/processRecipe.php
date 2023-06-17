d157943dd4a22d95c62dfdbfb03672a2fd324006
KauaDiniz6287 <125364596+KauaDiniz6287@users.noreply.github.com>
2023-06-17 20:19:37 -0300

    v1.0 FINAL (Fearless Falcon)

--- php/scripts/processRecipe.php
+++ php/scripts/processRecipe.php
@@ -8,9 +8,6 @@ if (!empty($_FILES['photo']['name'])){
     $destino = "../../database/imgRecipe/$photo";
 
     move_uploaded_file($origem, $destino);
-
-    echo "<h1>Deu certo!</h1>";
 } else {
     $photo = "vazio.jpg";
-    echo "<h1>Não resultou!</h1>";
 }
